ea0c51b6e7b08f1f9325f4a955cc4f33ba83fd7f
duduab <dudu.abitbul1@t-mobile.com>
2022-12-13 18:20:24 +0200

    initial commit

---
+++ Calculator/Producs.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Calculator
+{
+    public class Producs
+    {
+        [BsonElement("Trade_Item_Description")]
+        public string Trade_Item_Description { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Fats { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Carbohydrates { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Proteins { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float DietaryFiber { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float SaturatedFattyAcids { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Dessert { get; set; }
+
+        public ObjectId _id { get; set; }
+    }
+}
